0b8e93b7ad9a9c8d0d7f65a667c33585a66286c6
Victor Perez <vparmenta@uma.es>
2024-04-23 14:22:46 +0200

    themeSwitch + css changes

--- Dutify/src/components/themeSwitch/themeSwitch.jsx
+++ Dutify/src/components/themeSwitch/themeSwitch.jsx
@@ -1,11 +1,25 @@
-import "./themeSwitchStyle.css"
+import { useState } from "react";
+import "./themeSwitchStyle.css";
+import { BiMoon } from "react-icons/bi";
+import { BiSun } from "react-icons/bi";
 
 function ThemeSwitch() {
+  const [theme, setTheme] = useState("dark");
+
+  const switchTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
   return (
-    <button className="theme-toogle">
-        <svg>
-            
-        </svg>
+    <button
+      className="theme-toogle position-absolute top-50 translate-middle-y"
+      onClick={switchTheme}
+    >
+      {theme === "dark" ? (
+        <BiSun size={30} color="white"></BiSun>
+      ) : (
+        <BiMoon size={30} color="black"></BiMoon>
+      )}
     </button>
   );
 }
