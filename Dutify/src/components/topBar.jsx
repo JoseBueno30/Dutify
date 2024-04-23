4378083a25ca3fd08eb222d2c94adb9ee8cbcd00
Victor Perez <vparmenta@uma.es>
2024-04-23 13:20:19 +0200

    display flex + fast animation

--- Dutify/src/components/topBar.jsx
+++ Dutify/src/components/topBar.jsx
@@ -6,20 +6,18 @@ import ThemeSwitch from "./themeSwitch/themeSwitch";
 function TopBar() {
   return (
     <header className="position-relative topBar ps-2 pe-2">
-      <div className="position-relative top-50 start-0 translate-middle-y float">
-        <button
-          className="float-start pt-1 me-2 btn btn-secondary"
-          type="button"
-        >
+      <div className="position-relative mh-100 d-flex">
+        <button className="pt-1 m-2 btn btn-secondary" type="button">
           <BiArrowBack />
         </button>
-        <h1 className="float-start fs-3" style={{ color: "#655BE6" }}>
+        <h1 className="float-start fs-3 mt-2" style={{ color: "#655BE6" }}>
           DutyFy
         </h1>
-        <NavButton texto="Inicio"></NavButton>
-        <NavButton texto="Generos"></NavButton>
-        <NavButton texto="Listas"></NavButton>
-
+        <nav>
+          <NavButton texto="Inicio"></NavButton>
+          <NavButton texto="Generos"></NavButton>
+          <NavButton texto="Listas"></NavButton>
+        </nav>
         <ThemeSwitch></ThemeSwitch>
         <SearchBar></SearchBar>
       </div>
