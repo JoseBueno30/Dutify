6a0a417dad0ceb71d8bbf8ed4bc62fb664c201d8
adriantd3 <adriantorremocha@hotmail.com>
2024-04-24 19:50:56 +0200

    Reordenacion archivos

--- Dutify/src/main.jsx
+++ Dutify/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import ListCard from "./components/listCard";
+import ListCard from "./components/listCard/listCard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
