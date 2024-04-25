5eb5ed5f0bc56107232c642745d7acab5b1f539f
adriantd3 <adriantorremocha@hotmail.com>
2024-04-25 20:47:03 +0200

    GenreCard con fondo colores complementarios

--- Dutify/src/main.jsx
+++ Dutify/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import ListCard from "./components/listCard/listCard";
+import GenreCard from "./components/genreCard/genreCard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="d-flex">
-      <ListCard listName="NombreDeLista" listImage="/assets/mora_estrella.jpg"/>
+      <GenreCard genreName="Pop" background="pop"/>
+      <GenreCard genreName="Pop" background="pop"/>
     </div>
   </React.StrictMode>
 );
