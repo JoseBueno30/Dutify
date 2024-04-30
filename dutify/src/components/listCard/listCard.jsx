fed08abb70ba03bf3c62041201d5b604602a3e06
adriantd3 <adriantorremocha@hotmail.com>
2024-04-30 20:31:50 +0200

    Update ListButton

--- dutify/src/components/listCard/listCard.jsx
+++ dutify/src/components/listCard/listCard.jsx
@@ -1,15 +1,32 @@
-import "./styles/listCardStyle.css"
+import React, { useReducer} from "react";
+import "./styles/listCardStyle.css";
+
+function PlaceHolder(state, action) {
+  
+}
+
+function handleClick() {
+  dispatch({
+
+  });
+}
 
 function ListCard({ listName, listImage }) {
+
+  const [state, dispatch] = useReducer(PlaceHolder, []);
+
   return (
-    <div className="card text-bg-dark m-3" id="main_container" alt={listName + '_Imagen'}>
+    <div
+      className="card w-100 h-auto"
+      id="main_container"
+      alt={listName + "_Imagen"}
+    >
       <img src={listImage} className="card-img" />
       <div className="card-img-overlay title h-50">
-        <p className="card-title">{listName}</p>
+        <p>{listName}</p>
       </div>
     </div>
   );
 }
 
 export default ListCard;
-
