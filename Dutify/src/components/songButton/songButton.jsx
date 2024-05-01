370cb81d6d0b5faa3be3736e07a18c3090f78471
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2024-05-01 20:50:27 +0200

    songButton darkMode
    componente y estilos del boton de cancion para el modo oscuro. Falta la logica y usar el useContext para alternar los modos claro y oscuro

--- Dutify/src/components/songButton/songButton.jsx
+++ Dutify/src/components/songButton/songButton.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import { FaEllipsisVertical, FaPlay } from "react-icons/fa6";
 import "./songButtonStyle.css";
 
 
 export default function SongButton(){
-    cont 
+    
+    const[areOptionsVisible, setOptionsVisible] = useState(false);
+
+    const optionsClickHandler = (e) => {
+        setOptionsVisible(!areOptionsVisible)
+    }
+
+    const songClickHandler = (e) => {
+    }
+
     return(
             <>
-            <div className='song'>
-                <div className="playContainer">
+            <div className='song' onDoubleClick={songClickHandler}>
+                <div className="playContainer" onClick={songClickHandler}>
                     <image></image>
                     <div className="playButton"><FaPlay/></div>
                 </div>
@@ -20,15 +30,70 @@ export default function SongButton(){
                         </div>
                         <div className='album col'>album</div>
                         <div className='time col-3 col-md-2'>mm:ss</div>
-                        <div className='options col-md-1 col-2 '>
-                                <FaEllipsisVertical className="border"/>
+                        <div className='col-md-1 col-2 d-flex justify-content-center' onClick={optionsClickHandler}>
+                                <FaEllipsisVertical className="options"/>
                         </div>
                     </div>
                 </div>
                 
             </div>
-            <div className="border optionsLabel">a</div>
+            {areOptionsVisible &&
+                <OptionsLabel setOptionsVisible={setOptionsVisible}/>
+            }
             </>
-    
+    );
+}
+
+function OptionsLabel({setOptionsVisible}){
+
+    const[areListsVisible, setListsVisible] = useState(false);
+
+    const mouseLeaveHandler = (e) => {
+        setOptionsVisible(false);
+    }
+
+    const listsClickHandler = (e) => {
+        setListsVisible(!areListsVisible)
+    }
+
+    return(
+        <div onMouseLeave={mouseLeaveHandler}>
+            <div className="optionsLabel " >
+                <div className="option p-1">Añadir a canciones favoritas</div>
+                <div className="option d-flex justify-content-between" onClick={listsClickHandler}>
+                    <div className="p-1">Añadir a la lista</div>
+                    <div className="p-1">{">"}</div>
+                </div>
+            </div>
+            {areListsVisible &&
+                <ListsLabel setListsVisible={setListsVisible}/>
+            }
+        </div>
+    );
+}
+
+function ListsLabel({setListsVisible}){
+    const mouseLeaveHandler = (e) => {
+        setListsVisible(false);
+    }
+
+    const listClickHandler = (e) => {
+
+    }
+
+    const newListClickHandler = (e) => {
+        
+    }
+
+    return(
+        <>
+            <div className="listsLabel " onMouseLeave={mouseLeaveHandler}>
+                <div className="option p-1" onClick={listClickHandler}>Lista 1</div>
+                <div className="option p-1" onClick={listClickHandler}>Lista 2</div>
+                <div className="option p-1" onClick={listClickHandler}>Lista 3</div>
+                <div className="option p-1" onClick={newListClickHandler}>Nueva Lista</div>
+            </div>
+            
+        </>
     );
 }
