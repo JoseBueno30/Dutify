8f84fe12c5c7bb90ae6626468c2e7b6c35dc3823
Victor Perez <vparmenta@uma.es>
2024-05-01 19:41:23 +0200

    fix del merge

--- Dutify/src/App.jsx
+++ Dutify/src/App.jsx
@@ -1,11 +1,39 @@
-import TopBar from './components/topBar/topBar'
-import './index.css'
+import TopBar from "./components/topBar/topBar";
+import "./index.css";
+import ListCard from "./components/listCard/listCard";
+import GenreCard from "./components/genreCard/genreCard";
 
 function App() {
-
   return (
-    <TopBar></TopBar>
-  )
+    <>
+      <TopBar></TopBar>
+      <div className="d-flex">
+        <GenreCard genreName="Pop" background="pop" />
+        <GenreCard genreName="Rock" background="rock" />
+        <GenreCard genreName="Hip-Hop" background="hip-hop" />
+      </div>
+      <div className="d-flex">
+        <GenreCard genreName="Jazz" background="jazz" />
+        <GenreCard genreName="Reggaeton" background="reggaeton" />
+        <GenreCard genreName="Flamenco" background="flamenco" />
+      </div>
+      <div className="d-flex">
+        <GenreCard genreName="Techno" background="techno" />
+        <GenreCard genreName="Metal" background="metal" />
+        <GenreCard genreName="K-Pop" background="k-pop" />
+      </div>
+      <div className="d-flex">
+        <GenreCard genreName="Soul" background="soul" />
+        <GenreCard genreName="Clásica" background="clasica" />
+        <GenreCard genreName="Videojuegos" background="videojuegos" />
+      </div>
+      <div className="d-flex">
+        <GenreCard genreName="Anime" background="anime" />
+        <GenreCard genreName="Trap" background="trap" />
+        <GenreCard genreName="Latino" background="latino" />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
