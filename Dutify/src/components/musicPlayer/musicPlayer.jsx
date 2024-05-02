aa93520f166b6faf81b514baa3127b2bc3e9853e
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-05-02 11:13:15 +0200

    music player fixes

--- Dutify/src/components/musicPlayer/musicPlayer.jsx
+++ Dutify/src/components/musicPlayer/musicPlayer.jsx
@@ -1,5 +1,5 @@
 import "./musicPlayer.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function MusicPlayer() {
   const [playing, swtich] = useState(true);
@@ -8,11 +8,21 @@ function MusicPlayer() {
     swtich(!playing)
   };
 
+  useEffect(() => {
+    for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+      e.style.setProperty('--value', e.value);
+      e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+      e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+      e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+    }
+  }, []);
+
   return (
     <>
       <div className="position-absolute bottom-0 start-0 music-bar">
         <div className="music-container">
             <div className="song-container">
+              {/* TODO: reemplazar por información real de la canción */}
                 {/* Imagen de Cancion */}
                 <div className="simulate-image"></div>
                 {/* Texto de Artista */}
@@ -23,16 +33,16 @@ function MusicPlayer() {
             </div>
             {/* Barra de repreducción */}
             <div className="progresion-bar">
-                <input className="song-bar" type="range"></input>
+                <input className="styled-slider slider-progress" type="range"></input>
                 <div className="song-buttons">
-                    <img src="src\assets\previous-button.svg"></img>
+                    <img onPointerOver={} src="src\assets\previous-button.svg"></img>
                     {playing ? <img src="src\assets\play-button.svg" onClick={switchPlay}></img> : <img src="src\assets\stop-button.svg" onClick={switchPlay}></img>}
                     <img src="src\assets\next-button.svg"></img>
                 </div> 
             </div>
             {/* Barra de Soido */}
             <div className="sound-bar">
-                <input type="range"></input>
+                <input type="range" className="styled-slider slider-progress"></input>
             </div>
         </div>
       </div>
