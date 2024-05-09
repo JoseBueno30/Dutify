cd7b5660c4946f56801983a9ec45291743d8e013
adriantd3 <adriantorremocha@hotmail.com>
2024-05-09 23:43:36 +0200

    helpButton - Small fix

--- Dutify/src/components/topBar/helpButton/helpButton.jsx
+++ Dutify/src/components/topBar/helpButton/helpButton.jsx
@@ -2,9 +2,6 @@ import { GrCircleQuestion } from "react-icons/gr";
 import "../iconsStyle.css";
 
 function HelpButton({ visible }) {
-  function showHelp() {
-    //Activate help popup
-  }
   return (
     <div
       style={{ aspectRatio: "1/1", height: "80%" }}
@@ -16,7 +13,7 @@ function HelpButton({ visible }) {
         data-bs-toggle="modal"
         data-bs-target="#helpModal"
       > 
-        <GrCircleQuestion size={35} title="Mostrar ayuda" />
+        <GrCircleQuestion size={33} title="Mostrar ayuda" />
       </button>
     </div>
   );
