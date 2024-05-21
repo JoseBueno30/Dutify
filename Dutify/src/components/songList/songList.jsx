db833bf0cfbc2dd240123e6ea89ee64e7c9b6131
Victor Perez <vparmenta@uma.es>
2024-05-21 12:31:17 +0200

    Cargar todas las canciones de la playlist

--- Dutify/src/components/songList/songList.jsx
+++ Dutify/src/components/songList/songList.jsx
@@ -29,12 +29,11 @@ export default function SongList({tracks, playlistId}) {
 
   return (
     <div className="list container-fluid ">
-      {tracks.length>0 ? (<SongInfo/>) : (<></>)}
-      
+      <SongInfo/>      
       {/* PARA PLAYLIST */}
       {tracks.length>0 && playlistId ? (
           tracks.map((track) => (
-            track.track !== null ? <SongButton
+            track !== null ? <SongButton
             key={track.track.id}
             track={track.track}
             playlistId = {playlistId}
@@ -47,7 +46,7 @@ export default function SongList({tracks, playlistId}) {
       {/* PARA BUSQUEDA */}
       {tracks.length>0 && !playlistId ? (
           tracks.map((track) => (
-            track !== null ? <SongButton
+            track.track !== null ? <SongButton
             key={track.id}
             track={track}
             playLists={userPlayLists}
