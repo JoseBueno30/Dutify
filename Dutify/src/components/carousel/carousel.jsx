5de9e3161486939ce187977a9853b8e0c64bcbc4
Ezequiel Sánchez <124664474+ezee-sg@users.noreply.github.com>
2024-05-25 18:13:20 +0200

    Cambio de flechas del carrusel

--- Dutify/src/components/carousel/carousel.jsx
+++ Dutify/src/components/carousel/carousel.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './carouselStyle.css';
+import left_arrow from './left_arrow.svg';
+import right_arrow from './right_arrow.svg';
 
 const CustomCarousel = ({ lista, name, id }) => {
 
     const ClickHandler = (id) => {
         window.location.href = "/listas/playlist?playlistId=" + id;
     }
+
+    // Flecha personalizada
+    const CustomArrow = ({ onClick, direction }) => (
+        <button className={`custom-arrow custom-arrow-${direction}`} onClick={onClick}>
+            {direction === 'left' ? <img src={left_arrow} alt="Previous"/> : <img src={right_arrow} alt="Next"/>}
+        </button>
+    );
 
   return (
     <section className='carrusel-container' aria-label={name} id={id}>
@@ -22,6 +31,13 @@ const CustomCarousel = ({ lista, name, id }) => {
                 transitionTime={500}
                 centerMode
                 centerSlidePercentage={35}
+                // Flechas personalizadas
+                renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                    hasPrev && <CustomArrow onClick={onClickHandler} direction="left" />
+                }
+                renderArrowNext={(onClickHandler, hasNext, label) =>
+                    hasNext && <CustomArrow onClick={onClickHandler} direction="right" />
+                }
                 >
                 {lista.map((playlist) => (
                     <div key={playlist.id} className="carousel__item" onClick={()=>{ClickHandler(playlist.id)}}>
@@ -29,6 +45,7 @@ const CustomCarousel = ({ lista, name, id }) => {
                     </div>
                 ))}
             </Carousel>
+            <div className='carrusel-dot-section'/>
         </div>
     </section>
     
