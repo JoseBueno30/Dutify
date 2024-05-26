c79f631118cb7bb49e9770c3534e2fd6dc798c27
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2024-05-26 12:09:21 +0200

    popup time update

--- Dutify/src/App.jsx
+++ Dutify/src/App.jsx
@@ -119,7 +119,7 @@ function App() {
   const { getRootProps, onClickAway } = useSnackbar({
     onClose: handleClose,
     open,
-    autoHideDuration: 250000,
+    autoHideDuration: 2500,
   });
 
   const changeFeedback = (text) => {
