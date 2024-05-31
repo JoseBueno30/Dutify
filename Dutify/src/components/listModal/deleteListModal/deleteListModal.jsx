c2dae1d6f008d9a754b86bc0f3dc23a8b1f29614
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2024-05-31 17:16:10 +0200

    elimninar playlist arreglado

--- Dutify/src/components/listModal/deleteListModal/deleteListModal.jsx
+++ Dutify/src/components/listModal/deleteListModal/deleteListModal.jsx
@@ -5,7 +5,7 @@ import { unfollowPlaylist, sleep } from "../../../spotifyApi/SpotifyApiCalls";
 
 function DeleteListModal({ playlist }) {
   const comfirmDeleteClickHandler = (e) => {
-    unfollowPlaylist(playlist.id).then(() => {window.location.href = "/Listas"});
+    unfollowPlaylist(playlist).then(sleep(500).then(() => {window.location.href = "/Listas"}));
   };
 
   return (
